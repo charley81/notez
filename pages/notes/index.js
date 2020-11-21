125c438c726abf8ca352b00994e972d3165f3a33
ChrisKnowsDev <chrisharley81@gmail.com>
2020-11-20 20:00:42 -0500

    added themeUI, created nav component, added basic styling

--- pages/notes/index.js
+++ pages/notes/index.js
@@ -1,5 +1,38 @@
+/** @jsxRuntime classic */
+/** @jsx jsx */
 import React from 'react'
+import { jsx } from 'theme-ui'
+import Link from 'next/link'
 
-const Page = () => <div>Notes Index Path</div>
+export default () => {
+  const notes = new Array(15)
+    .fill(1)
+    .map((e, i) => ({ id: i, title: `This is my note ${i}` }))
 
-export default Page
+  return (
+    <div sx={{ variant: 'containers.page' }}>
+      <h1>My Notes</h1>
+
+      <div
+        sx={{
+          display: 'flex',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+          flexWrap: 'wrap',
+        }}
+      >
+        {notes.map(note => (
+          <div sx={{ width: '33%', p: 2 }}>
+            <Link key={note.id} href="/notes/[id]" as={`/notes/${note.id}`}>
+              <a sx={{ textDecoration: 'none', cursor: 'pointer' }}>
+                <div sx={{ variant: 'containers.card' }}>
+                  <strong>{note.title}</strong>
+                </div>
+              </a>
+            </Link>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
